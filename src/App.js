cbd7366b1b45f3ecb29a7ac5e97976d27fe943b2
kakon4366 <kakonbarman4366@gmail.com>
2022-03-20 21:40:44 +0600

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Flags from './Components/Flags/Flags';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Flags></Flags>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <small>
+                <p>&copy; Copyright by rest countries api - 2022</p>
+            </small>
+        </div>
+    );
+};
+
+export default Footer;

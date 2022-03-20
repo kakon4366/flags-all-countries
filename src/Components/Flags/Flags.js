6326542812161d461a4f63c6baf12ce6b977bfe5
kakon4366 <kakonbarman4366@gmail.com>
2022-03-20 21:05:27 +0600

    Rest Countries all data show fetch and total countries counting

--- src/Components/Flags/Flags.js
+++ src/Components/Flags/Flags.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Flags = () => {
+    const [flags, setFlags] = useState([]);
+
+    useEffect( () => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setFlags(data)) //asynchronous
+    }, [])
     
     return (
         <div>
-            <h1>This is Flags Components</h1>
+            <h1>All Countires Flags!!!</h1>
+            <hr />
+            <h4>Total Country: {flags.length}</h4>
         </div>
     );
 };

87d2dbaec857e4afca3c51e05b580ba807398ba9
kakon4366 <kakonbarman4366@gmail.com>
2022-03-20 18:31:53 +0600

    Flags Component Added

---
+++ src/Components/Flags/Flags.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Flags = () => {
+    
+    return (
+        <div>
+            <h1>This is Flags Components</h1>
+        </div>
+    );
+};
+
+export default Flags;

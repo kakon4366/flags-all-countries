bc8614edb3e55de9bb05c7915b40f0d2776552fc
kakon4366 <kakonbarman4366@gmail.com>
2022-04-05 20:36:07 +0600

    Input field added

--- src/Components/Flags/Flags.js
+++ src/Components/Flags/Flags.js
@@ -1,28 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import Flag from '../Flag/Flag';
-import './Flags.css';
+import React, { useEffect, useState } from "react";
+import Flag from "../Flag/Flag";
+import "./Flags.css";
 
 const Flags = () => {
-    const [flags, setFlags] = useState([]);
+	const [flags, setFlags] = useState([]);
 
-    useEffect( () => {
-        fetch('https://restcountries.com/v3.1/all')
-            .then(res => res.json())
-            .then(data => setFlags(data)) //asynchronous
-    }, [])
-    
-    return (
-        <div className='container'>
-            <h1>All Countires Flags!!!</h1>
-            <hr />
-            <h4>Total Country: {flags.length}</h4>
-            <div className='flags-container'>
-                {
-                    flags.map(flag => <Flag flag={flag}></Flag>)
-                }
-            </div>
-        </div>
-    );
+	useEffect(() => {
+		fetch("https://restcountries.com/v3.1/all")
+			.then((res) => res.json())
+			.then((data) => setFlags(data)); //asynchronous
+	}, []);
+
+	return (
+		<div className="container">
+			<h1>All Countires Flags!!!</h1>
+			<hr />
+			<input type="text" placeholder="Search Country" />
+			<h4>Total Country: {flags.length}</h4>
+			<div className="flags-container">
+				{flags.map((flag) => (
+					<Flag flag={flag}></Flag>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Flags;
